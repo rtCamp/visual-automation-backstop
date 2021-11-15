68a9bc39501126068f01cfe4fd40ac9396a66df4
pavanpatil1 <=>
2021-11-15 12:32:39 +0530

    Add new visual test case

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,108 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_rtmedia_homes_page_0__0_phone.png",
+        "test": "..\\bitmaps_test\\20211004-173443\\backstop_default_rtmedia_homes_page_0__0_phone.png",
+        "selector": "",
+        "fileName": "backstop_default_rtmedia_homes_page_0__0_phone.png",
+        "label": "rtmedia homes page",
+        "misMatchThreshold": 0.1,
+        "url": "",
+        "referenceUrl": "https://demo.rtmedia.io/",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "engineErrorMsg": "Protocol error (Page.navigate): Cannot navigate to invalid URL",
+        "error": "Reference file not found D:\\Automation\\dev-site-test\\backstop_data\\bitmaps_reference\\backstop_default_rtmedia_homes_page_0__0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_rtmedia_homes_page_0__1_tablet.png",
+        "test": "..\\bitmaps_test\\20211004-173443\\backstop_default_rtmedia_homes_page_0__1_tablet.png",
+        "selector": "",
+        "fileName": "backstop_default_rtmedia_homes_page_0__1_tablet.png",
+        "label": "rtmedia homes page",
+        "misMatchThreshold": 0.1,
+        "url": "",
+        "referenceUrl": "https://demo.rtmedia.io/",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "engineErrorMsg": "Protocol error (Page.navigate): Cannot navigate to invalid URL",
+        "error": "Reference file not found D:\\Automation\\dev-site-test\\backstop_data\\bitmaps_reference\\backstop_default_rtmedia_homes_page_0__1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_rtmedia_homes_page_0__2_Desktop.png",
+        "test": "..\\bitmaps_test\\20211004-173443\\backstop_default_rtmedia_homes_page_0__2_Desktop.png",
+        "selector": "",
+        "fileName": "backstop_default_rtmedia_homes_page_0__2_Desktop.png",
+        "label": "rtmedia homes page",
+        "misMatchThreshold": 0.1,
+        "url": "",
+        "referenceUrl": "https://demo.rtmedia.io/",
+        "expect": 0,
+        "viewportLabel": "Desktop",
+        "engineErrorMsg": "Protocol error (Page.navigate): Cannot navigate to invalid URL",
+        "error": "Reference file not found D:\\Automation\\dev-site-test\\backstop_data\\bitmaps_reference\\backstop_default_rtmedia_homes_page_0__2_Desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_rtmedia_memberss_page_0__0_phone.png",
+        "test": "..\\bitmaps_test\\20211004-173443\\backstop_default_rtmedia_memberss_page_0__0_phone.png",
+        "selector": "",
+        "fileName": "backstop_default_rtmedia_memberss_page_0__0_phone.png",
+        "label": "rtmedia memberss page",
+        "misMatchThreshold": 0.1,
+        "url": "",
+        "referenceUrl": "https://demo.rtmedia.io/members/",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "engineErrorMsg": "Protocol error (Page.navigate): Cannot navigate to invalid URL",
+        "error": "Reference file not found D:\\Automation\\dev-site-test\\backstop_data\\bitmaps_reference\\backstop_default_rtmedia_memberss_page_0__0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_rtmedia_memberss_page_0__1_tablet.png",
+        "test": "..\\bitmaps_test\\20211004-173443\\backstop_default_rtmedia_memberss_page_0__1_tablet.png",
+        "selector": "",
+        "fileName": "backstop_default_rtmedia_memberss_page_0__1_tablet.png",
+        "label": "rtmedia memberss page",
+        "misMatchThreshold": 0.1,
+        "url": "",
+        "referenceUrl": "https://demo.rtmedia.io/members/",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "engineErrorMsg": "Protocol error (Page.navigate): Cannot navigate to invalid URL",
+        "error": "Reference file not found D:\\Automation\\dev-site-test\\backstop_data\\bitmaps_reference\\backstop_default_rtmedia_memberss_page_0__1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_rtmedia_memberss_page_0__2_Desktop.png",
+        "test": "..\\bitmaps_test\\20211004-173443\\backstop_default_rtmedia_memberss_page_0__2_Desktop.png",
+        "selector": "",
+        "fileName": "backstop_default_rtmedia_memberss_page_0__2_Desktop.png",
+        "label": "rtmedia memberss page",
+        "misMatchThreshold": 0.1,
+        "url": "",
+        "referenceUrl": "https://demo.rtmedia.io/members/",
+        "expect": 0,
+        "viewportLabel": "Desktop",
+        "engineErrorMsg": "Protocol error (Page.navigate): Cannot navigate to invalid URL",
+        "error": "Reference file not found D:\\Automation\\dev-site-test\\backstop_data\\bitmaps_reference\\backstop_default_rtmedia_memberss_page_0__2_Desktop.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
