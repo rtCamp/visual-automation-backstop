c030b561fb206be716bc659e5e6e7c486a75f1e6
Pavan  Patil <44057535+pavanpatil1@users.noreply.github.com>
2022-03-30 16:26:50 +0530

    Update basicConfig.js

--- test/basicConfig.js
+++ test/basicConfig.js
@@ -1,10 +1,5 @@
-const baseUrl = "https://example.com" // Replace the value "https://example.com" by the base URL of the website you want to test.
-const referenceUrl = "https://reference.com" //Optional URL, Replace the value "https://reference.com" by the reference URL of the website you want to compare with.
-
 
 const projectId = "rtMedia Visual test"; // Replace the value "sample project" by the id of your project. It can be any string (e.g., "my-website").
-
-let config = []; 
 
 // Leave the below array as is if you want to test your website using the viewports listed below.
 // The suported viewports are: phone (320px X 480px), tablet (1024px X 768px), and desktop (1280px X 1024px).
@@ -17,9 +12,6 @@ const viewports = [
 ];
 
 module.exports = {
-  baseUrl,
   projectId,
-  viewports,
-  referenceUrl, //Optional
-  config
+  viewports
 };
