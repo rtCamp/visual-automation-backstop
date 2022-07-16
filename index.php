852c61161d8d63024635e9d6bbe707334c7ec43a
Paul David Clark <pd@pd.cm>
2022-07-15 18:07:24 -0700

    Convert index.php to WordPress plugin. Add shortcode [visual] with form and progress tracker. @todo: Pass project_id from PHP to Node via CLI argument. This version allows only one test per day.

--- index.php
+++ index.php
@@ -1,119 +1,473 @@
-<form action="/" method="GET">
-	<input type="url" name="live" value="<?php echo esc_url_raw( $_GET['live'] ); ?>" placeholder="https://site.live" />
-	<input type="url" name="dev" value="<?php echo esc_url_raw( $_GET['dev'] ); ?>" placeholder="https://site.dev" />
-	<button>Test</button>
-</form>
 <?php
+/**
+ * Plugin Name: Visual
+ * Description: Adds <code>[visual]</code> shortcode for visual testing form.
+ * Plugin URI: https://github.com/rtcamp/visual-automation-backstop
+ * Version: 1
+ * Author: rtCamp
+ * Author URI: https://rtcamp.com
+ */
 
-// Each of the items below should be an individual AJAX call with a wrapper that monitors progress.
-set_time_limit( -1 );
+namespace Visual;
 
-if ( empty( $_GET['live'] ) || empty( $_GET['dev'] ) ) {
-	return;
-}
+add_action(
+	'init',
+	function(){
+		add_shortcode(
+			'visual',
+			function( $a ) {
+				wp_enqueue_script( 'jquery' );
 
-// XML Sitemap to CSV
-if ( ! file_exists( __DIR__ . '/data/' . $project_id . '.csv' ) ) {
-	$urls = [];
+				ob_start();
+				?>
+<form id="visual-form">
+	<input type="url" name="live" placeholder="https://site.live" />
+	<input type="url" name="dev" placeholder="https://site.dev" />
+	<button><?php \esc_html_e( 'Test', 'visual' ); ?></button>
+</form>
+<div id="visual-queue">
+	<?php
+	$q = new \WP_Query([
+		'post_type' => 'visual',
+		'post_status' => 'publish',
+		'posts_per_page' => 100,
+		'orderby' => 'ID',
+		'order' => 'DESC',
+	]);
 
-	$sitemap = file_get_contents(
-		trim( esc_url_raw( $_GET['live'] ), '/ ' ) . '/wp-sitemap.xml' 
-	);
+	while( $q->have_posts() ) {
+		$q->the_post();
 
-	$sitemap_dom = new \DomDocument();
-	@$sitemap_dom->loadHTML(
-		mb_convert_encoding( $sitemap, 'HTML-ENTITIES', 'UTF-8' )
-	);
-
-	try{
-		foreach( $sitemap_dom->getElementsByTagName('loc') as $s ) {
-			$sub_sitemap = file_get_contents( $s->textContent );
-
-			$sub_sitemap_dom = new \DomDocument();
-			@$sub_sitemap_dom->loadHTML(
-				mb_convert_encoding( $sub_sitemap, 'HTML-ENTITIES', 'UTF-8' )
-			);
-
-			foreach( $sub_sitemap_dom->getElementsByTagName('loc') as $l ) {
-
-				if ( in_array(
-					pathinfo( $l->textContent, PATHINFO_EXTENSION ),
-					[ 'jpg', 'jpeg', 'png', 'gif', 'svg', 'mp4' ]
-				) ) {
-					continue;
-				}
-
-				$urls[] = $l->textContent;
-			}
+		$status = get_post_meta( get_the_ID(), 'status', true );
+		if ( empty( $status ) ) {
+			$status = 'processing';
 		}
-	}catch( \Expression $e ) {
 
+		if ( 'processing' === $status ) {
+			printf(
+				'<article data-id="%d" data-status="%s"><a href="%s">%s</a> %s</article>',
+				get_the_ID(),
+				esc_attr( $status ),
+				esc_url( get_the_permalink() ),
+				esc_html( get_the_title() ),
+				wp_kses_post( get_the_content() )
+			);
+		}else {
+			$url = plugins_url( sprintf(
+				'backstop_data/%s/html_report/index.html',
+				get_post_meta( get_the_ID(), 'project_id', true )
+			), __FILE__ );
+
+			printf(
+				'%s <a href="%s">%s</a>',
+				get_the_title(),
+				esc_url( $url  ),
+				__( 'Report', 'visual' )
+			);
+		}
+	}
+	wp_reset_postdata();
+	?>
+</div>
+				<?php
+				return ob_get_clean();
+			}
+		);
+
+		register_post_type( 'visual', [
+			'labels'             => [
+				'name'                  => _x( 'Visual', 'Post type general name', 'visual' ),
+				'singular_name'         => _x( 'Visual', 'Post type singular name', 'visual' ),
+				'menu_name'             => _x( 'Visuals', 'Admin Menu text', 'visual' ),
+				'name_admin_bar'        => _x( 'Visual', 'Add New on Toolbar', 'visual' ),
+				'add_new'               => __( 'Add New', 'visual' ),
+				'add_new_item'          => __( 'Add New Visual', 'visual' ),
+				'new_item'              => __( 'New Visual', 'visual' ),
+				'edit_item'             => __( 'Edit Visual', 'visual' ),
+				'view_item'             => __( 'View Visual', 'visual' ),
+				'all_items'             => __( 'All Visuals', 'visual' ),
+				'search_items'          => __( 'Search Visuals', 'visual' ),
+				'parent_item_colon'     => __( 'Parent Visuals:', 'visual' ),
+				'not_found'             => __( 'No Visuals found.', 'visual' ),
+				'not_found_in_trash'    => __( 'No Visuals found in Trash.', 'visual' ),
+				'featured_image'        => _x( 'Visual Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'visual' ),
+				'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'visual' ),
+				'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'visual' ),
+				'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'visual' ),
+				'archives'              => _x( 'Visual archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'visual' ),
+				'insert_into_item'      => _x( 'Insert into Visual', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'visual' ),
+				'uploaded_to_this_item' => _x( 'Uploaded to this Visual', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'visual' ),
+				'filter_items_list'     => _x( 'Filter Visuals list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'visual' ),
+				'items_list_navigation' => _x( 'Visual list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'visual' ),
+				'items_list'            => _x( 'Visual list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'visual' ),
+			],
+			'description'        => __( 'Visual web site tests.', 'visual' ),
+			'public'             => is_user_logged_in(),
+			'publicly_queryable' => is_user_logged_in(),
+			'exclude_from_search'=> ! current_user_can( 'edit_others_posts' ),
+			'show_ui'            => current_user_can( 'edit_others_posts' ),
+			'show_in_menu'       => current_user_can( 'edit_others_posts' ),
+			'query_var'          => is_user_logged_in(),
+			'rewrite'            => [ 'slug' => 'visual' ],
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => false,
+			'menu_position'      => 20,
+			'supports'           => [ 'title', 'editor', 'author', 'thumbnail' ],
+			'taxonomies'         => [],
+			'show_in_rest'       => true,
+			'rest_base'          => 'visual',
+			'rest_controller_class' => 'WP_REST_Posts_Controller',
+			'show_in_graphql' => current_user_can( 'edit_others_posts' ),
+			'graphql_single_name' => 'visual',
+			'graphql_plural_name' => 'visuals',
+		] );
+	}
+);
+
+add_action(
+	'wp_footer',
+	function(){
+		?>
+<script>
+jQuery(document).ready( function($){
+	$('#visual-form').on('submit', function(e){
+		e.preventDefault();
+
+		let $pending_visual = $('<article>');
+
+		$.ajax({
+			method: 'POST',
+			url:  '<?php echo esc_url_raw( rest_url( 'visual/v1/start-test' ) ); ?>',
+			data: {
+				live: $('#visual-form input[name="live"]').val(),
+				dev: $('#visual-form input[name="dev"]').val()
+			},
+			beforeSend: function ( xhr ) {
+				xhr.setRequestHeader( 'X-WP-Nonce', '<?php echo wp_create_nonce( 'wp_rest' ); ?>' );
+
+				$pending_visual
+					.text(
+						$('#visual-form input[name="live"]').val() + ' <> ' + $('#visual-form input[name="dev"]').val()
+					);
+
+				$('#visual-queue').prepend( $pending_visual );
+
+			},
+			error: function( r ) {
+				if ( 403 === r.status ) {
+					alert( 'You are not currently logged in. Please refresh the page to log in again.' );
+				} 
+			},
+			success : function( r ) {
+				$pending_visual
+					.html('')
+					.attr('data-id', r.id )
+					.attr('data-status', r.status )
+					.append(
+						$('<a>')
+							.attr('href', r.permalink )
+							.text( r.title )
+					);
+			}
+		});
+	});
+
+	setInterval(
+		function(){
+			$('#visual-queue article[data-status="processing"]').each( function( index ){
+				let $article = $(this);
+				$.ajax({
+					method: 'GET',
+					url:  '<?php echo esc_url_raw( rest_url( 'wp/v2/visual/' ) ); ?>' + $(this).attr('data-id'),
+					data: {},
+					beforeSend: function ( xhr ) {
+						xhr.setRequestHeader( 'X-WP-Nonce', '<?php echo wp_create_nonce( 'wp_rest' ); ?>' );
+					},
+					error: function( r ) {
+						if ( 403 === r.status ) {
+							alert( 'You are not currently logged in. Please refresh the page to log in again.' );
+						} 
+					},
+					success : function( r ) {
+						if ( -1 !== r.content.rendered.indexOf('Report') ) {
+							$article.html( r.title.rendered + ': ' + r.content.rendered )
+						}else {
+							$article.find('a').text( r.title.rendered + ': ' + r.content.rendered );
+						}
+					}
+				});
+			} );
+		},
+		6000
+	);
+});
+</script>
+		<?php
+	},
+	1000
+);
+
+add_action(
+	'rest_api_init',
+	function(){
+		\register_rest_route(
+			'visual/v1',
+			'/start-test',
+			[
+				'methods' => [ 'GET', 'POST' ],
+				'callback' => function( $request ) {
+					global $post;
+
+					$live_url = $request->get_param( 'live' );
+					$dev_url = $request->get_param( 'dev' );
+					$post_title = $live_url . ' <> ' . $dev_url;
+
+					$todays_visual = get_posts([
+						'post_type' => 'visual',
+						'post_title' => $post_title,
+						'date_query' => array(
+							'after' => 'today',
+							'inclusive'         => true,
+						),
+						'posts_per_page' => 1,
+					]);
+
+					if ( ! empty( $todays_visual ) ) {
+						$post = $todays_visual[0];
+					}else {
+						$id = \wp_insert_post( [
+							'post_title' => $post_title,
+							'post_content' => '',
+							'post_type' => 'visual',
+							'post_status' => 'publish',
+						] );
+
+						date_default_timezone_set( wp_timezone_string() );
+
+						update_post_meta( $id, 'live_url', esc_url_raw( $live_url ) );
+						update_post_meta( $id, 'dev_url', esc_url_raw( $dev_url ) );
+						update_post_meta( $id, 'project_id', 'project-' . date('n-j-Y') );
+						update_post_meta( $id, 'status', 'processing' );
+
+						\clean_post_cache( $id );
+						$post = get_post( $id );
+						\setup_postdata( $post );
+					}
+
+					add_action(
+						'shutdown',
+						function() use ( $post ) {
+
+							xml_sitemap_to_csv( $post );
+							start_backstop_reference( $post );
+
+						}
+					);
+
+					return [
+						'id' => intval( $post->ID ),
+						'title' => $post->post_title,
+						'permalink' => \get_permalink( $post->ID ),
+						'status' => \get_post_meta( $post->ID, 'status', true ),
+					];
+				},
+			]
+		);
+	}
+);
+
+function xml_sitemap_to_csv( $post ) {
+	$project_id = get_post_meta( $post->ID, 'project_id', true );
+	$live_url = get_post_meta( $post->ID, 'live_url', true );
+	$dev_url = get_post_meta( $post->ID, 'dev_url', true );
+
+	if ( ! file_exists( __DIR__ . '/data/' . $project_id . '.csv' ) ) {
+		$urls = [];
+
+		// @todo: convert to wp_remote_get() and check for 404.
+		$sitemap = file_get_contents(
+			trim( esc_url_raw( $live_url ), '/ ' ) . '/wp-sitemap.xml' 
+		);
+
+		$sitemap_dom = new \DomDocument();
+		@$sitemap_dom->loadHTML(
+			mb_convert_encoding( $sitemap, 'HTML-ENTITIES', 'UTF-8' )
+		);
+
+		try{
+			foreach( $sitemap_dom->getElementsByTagName('loc') as $s ) {
+				$sub_sitemap = file_get_contents( esc_url_raw( $s->textContent ) );
+
+				$sub_sitemap_dom = new \DomDocument();
+				@$sub_sitemap_dom->loadHTML(
+					mb_convert_encoding( $sub_sitemap, 'HTML-ENTITIES', 'UTF-8' )
+				);
+
+				foreach( $sub_sitemap_dom->getElementsByTagName('loc') as $l ) {
+
+					if ( in_array(
+						pathinfo( $l->textContent, PATHINFO_EXTENSION ),
+						[ 'jpg', 'jpeg', 'png', 'gif', 'svg', 'mp4' ]
+					) ) {
+						continue;
+					}
+
+					$urls[] = $l->textContent;
+				}
+			}
+		}catch( \Expression $e ) {
+
+		}
+
+		$fp = fopen( __DIR__ . '/data/' . $project_id . '.csv', 'w');
+
+		fputcsv($fp, [
+			'label','referenceUrl','url',
+		]);
+
+		foreach ($urls as $key => $url) {
+			// if ( $key > 10 ) { continue; } // limit URL count for shorter test runs.
+			fputcsv($fp, [
+				sanitize_title( $url ),
+				esc_url_raw( $url ),
+				esc_url_raw( str_replace(
+					$live_url,
+					$dev_url,
+					$url
+				) )
+			]);
+		}
+		fclose($fp);
 	}
 }
 
-$project_id = 'project-' . date('n-j-Y');
 
-$fp = fopen( __DIR__ . '/data/' . $project_id . '.csv', 'w');
+function start_backstop_reference( $post ) {
+	$prefix = 'export PATH="/opt/homebrew/bin/:$PATH" && cd ' . __DIR__ . ' && '; // Assume node installed with homebrew.
 
-fputcsv($fp, [
-	'label','referenceUrl','url',
-]);
-
-foreach ($urls as $url) {
-    fputcsv($fp, [
-    	md5( $url ),
-    	$url,
-    	str_replace(
-    		$_GET['live'],
-    		$_GET['dev'],
-    		$url
-    	)
-    ]);
+	// @todo: Pass project_id as CLI argument.
+	$command = $prefix . 'npm run reference:csv 2>&1';
+	$output = shell_exec( $command );
 }
-fclose($fp);
 
-// Backstop test
+function start_backstop_test( $post ) {
+	$project_id = get_post_meta( $post->ID, 'project_id', true );
 
-$prefix = 'export PATH="/opt/homebrew/bin/:$PATH" && cd ' . __DIR__ . ' && ';
+	if ( ! is_dir(
+		sprintf(
+			'%s/backstop_data/%s/bitmaps_test',
+			__DIR__,
+			$project_id
+		)
+	) ) {
+		$prefix = 'export PATH="/opt/homebrew/bin/:$PATH" && cd ' . __DIR__ . ' && '; // Assume node installed with homebrew.
 
-$command = $prefix . 'npm run reference:csv 2>&1';
-$output = shell_exec( $command );
+		// @todo: Pass project_id as CLI argument.
+		$command = $prefix . 'npm run test:csv 2>&1';
+		$output = shell_exec( $command );
 
-echo '<pre>';
-print_r( $command );
-print_r( $output );
-echo '</pre>';
-
-foreach ( (array) explode( PHP_EOL, $output ) as $line ) {
-	if ( false !== strpos( $line, 'ProjectID|' ) ) {
-		$project_id = explode( '|', $line )[1];
+		return false;
+	}else {
+		return true;
 	}
+
 }
 
-sleep( 1 );
+add_filter(
+	'the_content',
+	function( $c ) {
+		if ( 'visual' !== get_post_type() ) {
+			return $c;
+		}
+		$status = get_post_meta( get_the_ID(), 'status', true );
+		$project_id = get_post_meta( get_the_ID(), 'project_id', true );
 
-// Backstop reference
+		if ( 'processing' === $status ) {
+			if ( ! is_dir(
+				sprintf(
+					'%s/backstop_data/%s/html_report',
+					__DIR__,
+					$project_id
+				)
+			) ) {
+				// Current files
+				$backstop_files_in_progress = glob(
+					sprintf(
+						'%s/backstop_data/%s/bitmaps_reference/*',
+						__DIR__,
+						$project_id
+					)
+				);
 
-$command = $prefix . 'npm run test:csv 2>&1';
-$output = shell_exec( $command );
+				$backstop_test_files_in_progress = glob(
+					sprintf(
+						'%s/backstop_data/%s/bitmaps_test/*',
+						__DIR__,
+						$project_id
+					)
+				);
 
-echo '<pre>';
-print_r( $command );
-print_r( $output );
-echo '</pre>';
+				// Expected files
+				$csv_file = __DIR__ . '/data/' . $project_id . '.csv';
+				$file = new \SplFileObject( $csv_file, 'r');
+				$file->seek( PHP_INT_MAX );
+				$rows_in_csv = $file->key() - 1;
+				$expected_files = $rows_in_csv * 3; // 3 === Number of variations set in backstop. @todo get from config.
 
-$url = sprintf(
-	'%s://%s/backstop_data/%s/html_report/index.html',
-	'on' === $_SERVER['HTTPS'] ? 'https' : 'http',
-	$_SERVER['HTTP_HOST'],
-	$project_id
+				if (
+					$expected_files === count( $backstop_files_in_progress )
+					&& ! is_dir(
+						sprintf(
+							'%s/backstop_data/%s/bitmaps_test',
+							__DIR__,
+							$project_id
+						)
+					)
+				) {
+					$post = get_post( get_the_ID() );
+					add_action(
+						'shutdown',
+						function() use ( $post ) {
+							start_backstop_test( $post );
+						}
+					);
+				}
+			}else {
+				update_post_meta( get_the_ID(), 'status', 'complete' );
+
+				$url = plugins_url( sprintf(
+					'backstop_data/%s/html_report/index.html',
+					$project_id
+				), __FILE__ );
+
+				return sprintf(
+					'<a href="%s">%s</a>',
+					esc_url( $url  ),
+					__( 'Report', 'visual' )
+				);
+			}
+
+			return sprintf(
+				'%s of %s references processed. %s of %s tests processed.',
+				number_format( count( $backstop_files_in_progress ) ),
+				number_format( $expected_files ),
+				number_format( count( $backstop_test_files_in_progress ) ),
+				number_format( $expected_files )
+			) . $c;
+		}else {
+			$url = plugins_url( sprintf(
+				'backstop_data/%s/html_report/index.html',
+				$project_id
+			), __FILE__ );
+
+			return sprintf(
+				'<a href="%s">%s</a>',
+				esc_url( $url  ),
+				__( 'Report', 'visual' )
+			);
+		}
+
+		return $c;
+	}
 );
-printf(
-	'<a href="%s">%s</a>',
-	$url,
-	$url
-);
-
-function esc_url_raw( $url ) {
-	return $url; // No proper escaping anywhere in this file.
-}
