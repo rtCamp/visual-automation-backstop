6122588887c198a1d9f8936959e258e701e45850
pooja-muchandikar <pooja.muchandikar@rtcamp.com>
2023-05-29 15:09:58 +0530

    Add Sitemap.xml feature

---
+++ bin/sitemapurls.js
@@ -0,0 +1,42 @@
+const Sitemapper = require("sitemapper");
+const fs = require('fs');
+const { checkPrimeSync } = require("crypto");
+
+
+const sitemap = new Sitemapper();
+
+var siteURLS = [];
+
+sitemap.fetch("https://rtcamp.com/sitemap_index.xml").then(function (sites) {
+  const urlValue = Object.values(sites);
+
+  
+  for (var i = 0; i < urlValue.length; i++) {
+    if (Array.isArray(urlValue[i])) {
+      var innerArray = urlValue[i];
+      for (var j = 0; j < innerArray.length; j++) {
+        if (typeof innerArray[j] === "string") {
+          var urlParts = innerArray[j].split("/"); // Split the URL by '/'
+          var slugParts = urlParts.slice(3); // Exclude the domain and first two path segments
+          var fullSlug = slugParts.join("/"); // Join the slug parts back together with '/'
+          siteURLS.push(fullSlug);
+
+        }
+      }
+    }
+  }
+
+  const csvContent = siteURLS.join(',');
+
+fs.writeFile('output.csv', csvContent, function(err) {
+  if (err) {
+    console.error('Error writing CSV file:', err);
+  } else {
+    console.log('CSV file generated successfully!');
+  }
+});
+
+
+})
+
+
